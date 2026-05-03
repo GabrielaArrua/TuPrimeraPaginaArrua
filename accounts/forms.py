0a45b5f76088b1732fdf509253e97372fc7952de
Gabriela Arrúa <gabrielaarrua98@gmail.com>
2026-05-03 18:40:07 -0300

    Versión final lista para entrega

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,15 +1,36 @@
-from django import forms 
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class RegistroForm (UserCreationForm):
-    email = forms.EmailField()
+class RegistroForm(UserCreationForm):
+
+    username = forms.CharField(
+        label="Usuario",
+        widget=forms.TextInput(attrs={'placeholder': 'Nombre de usuario'})
+    )
+
+    email = forms.EmailField(
+        label="Correo electrónico",
+        widget=forms.EmailInput(attrs={'placeholder': 'Correo electrónico'})
+    )
+
+    password1 = forms.CharField(
+        label="Contraseña",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Contraseña'}),
+        help_text=""
+    )
+
+    password2 = forms.CharField(
+        label="Repetir contraseña",
+        widget=forms.PasswordInput(attrs={'placeholder': 'Repetir contraseña'}),
+        help_text=""
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-
+from django import forms
 from .models import Perfil
 
 class EditarPerfilForm(forms.ModelForm):
@@ -20,3 +41,9 @@ class EditarPerfilForm(forms.ModelForm):
     class Meta:
         model = Perfil
         fields = ['avatar', 'biografia', 'fecha_de_nacimiento']
+
+        widgets = {
+            'fecha_de_nacimiento': forms.DateInput(
+                attrs={'type': 'date'}
+            )
+        }
